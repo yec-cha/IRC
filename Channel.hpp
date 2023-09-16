3554f07625d5eba30c466f4b1c7dc26378ba7094
Chanmuk Jung <chanmukjung@Chanmukui-MacBookAir.local>
2023-09-16 14:54:35 +0900

    FIX: methods

--- Channel.hpp
+++ Channel.hpp
@@ -12,20 +12,20 @@ class Channel {
 private:
 	std::string name_;
 	std::string topic_;
-	std::string pass_;
-	std::map<int, User&> list_;
+	std::string password_;
+	std::map<int, User&> users_;
 	int operator_;
-	int size_;
+	// int mode_;
 
 public:
 	void printInfo() const {
 		std::cout << "[CHANNEL INFO]" << std::endl;
 		std::cout << "NAME: " << this->name_ << std::endl;
 		std::cout << "TOPIC: " << this->topic_ << std::endl;
-		std::cout << "PASS: " << this->pass_ << std::endl;
+		std::cout << "PASS: " << this->password_ << std::endl;
 		
 		std::cout << " - LIST" << std::endl;
-		for (std::map<int, User&>::const_iterator iter = list_.begin(); iter != this->list_.end(); ++iter) {
+		for (std::map<int, User&>::const_iterator iter = users_.begin(); iter != this->users_.end(); ++iter) {
 			std::cout << "FD: " << iter->first << ", USER NAME: " << iter->second.getUserName() << std::endl;
 		}
 		std::cout << " -" << std::endl;
@@ -46,18 +46,18 @@ public:
 	*/
 	void sendPRIVMSG(const std::vector<std::string>& parameters, std::vector<User>::iterator& iterUser) {
 		(void)iterUser;
-		// std::map<int, User&>::iterator first_ = list_.begin();
-		// std::map<int, User&>::iterator last_ = list_.end();
+		// std::map<int, User&>::iterator first_ = users_.begin();
+		// std::map<int, User&>::iterator last_ = users_.end();
 		// for (; first_ != last_ < ++first_) {
 		// 	send(first_->first, parameters[1].c_str(), parameters[1].length(), 0);
 		// }
 		// std::string messages;
 		this->printInfo();
-		// for (std::map<int, User&>::iterator iter = this->list_.begin(); iter != this->list_.end(); ++iter) {
+		// for (std::map<int, User&>::iterator iter = this->users_.begin(); iter != this->users_.end(); ++iter) {
 		// 	// std::string messages = parameters[1];
 		// 	send(iter->first, parameters[1].c_str(), parameters[1].length(), 0);
 		// }
-		for (std::map<int, User&>::iterator iter = this->list_.begin(); iter != this->list_.end(); ++iter) {
+		for (std::map<int, User&>::iterator iter = this->users_.begin(); iter != this->users_.end(); ++iter) {
 			std::string privmsg = ":" + iterUser->getNickName() + "!" + iterUser->getUserName() + "@" + "127.0.0.1" + " " + "PRIVMSG" + " " + parameters[0] + " " + parameters[1] + "\n";
 			send(iter->first, privmsg.c_str(), privmsg.length(), 0);
 		}
@@ -73,22 +73,19 @@ public:
 		// printInfo();
 		std::string str = ":" + iterUser->getNickName() + "!" + iterUser->getNickName() + "@" + "127.0.0.1" + " " + "JOIN" +  " " + this->name_ + "\n";
 		send(4, str.c_str(), str.length(), 0);
-		
 	}
 
-	Channel(const std::string &name, User& user) : name_(name), size_(1) {
+	Channel(const std::string &name, User& user) : name_(name) {
 		addUser(user);
 		setOperator(user.getSocket());
 	}
 
 	void addUser(User& user) {
-		this->list_.insert(std::pair<int, User&>(user.getSocket(), user));
-		++this->size_;
+		this->users_.insert(std::pair<int, User&>(user.getSocket(), user));
 	}
 
 	void delUser(User& user) {
-		this->list_.erase(user.getSocket());
-		--this->size_;
+		this->users_.erase(user.getSocket());
 	}
 
 	void setTopic(const std::string& topic) {
@@ -111,16 +108,19 @@ public:
 		return this->topic_;
 	}
 	
-	const std::string& getPass() const {
-		return this->pass_;
-	}
-
-	int getOperator() const {
-		return this->operator_;
+	const std::string& getPassword() const {
+		return this->password_;
 	}
 
 	int getSize() const {
-		return this->size_;
+		return this->users_.size();
+	}
+
+	// bool checkOperator(const User& user) const {
+	bool checkOperator(User& user) const {
+		if (this->operator_ == user.getSocket())
+			return true;
+		return false;
 	}
 };
 
